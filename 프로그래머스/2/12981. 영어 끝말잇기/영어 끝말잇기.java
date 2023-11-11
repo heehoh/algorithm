d58d03ef07921ad63f295232654908991595ac05
hujeong <114281631+heehoh@users.noreply.github.com>
2023-11-11 21:12:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,25 +1,20 @@
+import java.util.HashSet;
+
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        int min = 101;
+        HashSet<String> set = new HashSet<String>();
+        
+        set.add(words[0]);
         for (int i=0; i < words.length - 1; i++) {
             String str1 = words[i];
             String str2 = words[i+1];
-            if (str1.charAt(str1.length() - 1) != str2.charAt(0)) {
+            set.add(str2);
+            if (set.size() == i + 1 || 
+                str1.charAt(str1.length() - 1) != str2.charAt(0)) {
                 answer[0] = (i+1) % n + 1;
                 answer[1] = (i+1) / n + 1;
-                min = i + 1;
-                break ;
-            }
-        }
-        for (int i=0; i < words.length - 1; i++) {
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    if (min > j) {
-                        answer[0] = j % n + 1;
-                        answer[1] = j / n + 1;
-                    }
-                }
+                break;
             }
         }
         return answer;
