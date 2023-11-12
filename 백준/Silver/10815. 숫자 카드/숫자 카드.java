5c788d84e34f4b93c4df2c51793f61edb6fce95d
hujeong <114281631+heehoh@users.noreply.github.com>
2023-11-12 19:32:29 +0900

    [Silver V] Title: 숫자 카드, Time: 3540 ms, Memory: 163536 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.TreeSet;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        TreeSet<Integer> set = new TreeSet<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
         
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
