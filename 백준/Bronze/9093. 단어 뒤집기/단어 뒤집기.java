aeb2db456de0b3c1196dbaccad16ef285da12c41
hujeong <114281631+heehoh@users.noreply.github.com>
2023-11-15 00:15:35 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 560 ms, Memory: 53704 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        for (int i=0; i < n; ++i) {
-            String s = sc.nextLine();
-            String[] word = s.split(" ");
-            StringBuilder newString = new StringBuilder();
-            for (int j = 0; j < word.length; ++j) {
-                StringBuilder reversed = new StringBuilder(word[j]).reverse();
-                newString.append(reversed.toString() + " ");
+    public static void main(String[] args) throws IOException {
+        StringBuilder answer = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i=0; i<n; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()) {
+                StringBuilder sb = new StringBuilder(st.nextToken());
+                answer.append(sb.reverse() + " ");
             }
-            System.out.println(newString.toString());
+            answer.append("\n");
         }
+        System.out.print(answer);
     }
 }
