fcc66bc27944d605f03f8e545a82c6e7e50e5ec1
hujeong <114281631+heehoh@users.noreply.github.com>
2023-11-18 09:07:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 264 ms, Memory: 20056 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        System.out.println(st.countTokens());
+    }
+}
