6e0d70adf7fcd9142527e25e7c0716d58bd779f3
hujeong <114281631+heehoh@users.noreply.github.com>
2023-11-23 13:57:04 +0900

    [Bronze II] Title: 분해합, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int len = getLen(n);
         int a = 0;
-        for (int i = 1; i < n; ++i) {
+        for (int i = n - 9 * len; i < n; ++i) {
             if (n == i + rec(i)) {
                 a = i;
                 break;
@@ -17,5 +18,13 @@ public class Main {
         if (i == 0)
             return 0;
         return rec(i /10) + i%10;
+    }
+    public static int getLen(int n) {
+        int len = 0;
+        while (n != 0) {
+            n /= 10;
+            ++len;
+        }
+        return len;
     }
 }
