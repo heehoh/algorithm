61915c62396ebd52c0cf7be2b043fa5f0b72d168
hujeong <114281631+heehoh@users.noreply.github.com>
2024-03-27 13:42:07 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a, b, v; cin >> a >> b >> v;
+    int day = 1;
+    day += (v - a) / (a - b);
+    if((v-a)%(a-b) != 0)
+    {
+        ++day;
+    }
+    if (a >= v)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << day;
+    }
+    return 0;
+}
