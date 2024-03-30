def5db26b39c5a7aa2a3a98914f5e5fa3b4492bb
hujeong <114281631+heehoh@users.noreply.github.com>
2024-03-30 20:04:10 +0900

    틀린 이유

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -14,7 +14,7 @@ int main() {
         arr.push_back(length);
     }
  
-    // min max 구하기
+    // max 구하기 -> max를 구하는 이유 3 3 일때 1000 1000 1이라 했을 때 정답이 500이 나와야 함
     int max = 0;
     for (int i = 0; i < arr.size(); ++i) {
         if (max < arr[i])
