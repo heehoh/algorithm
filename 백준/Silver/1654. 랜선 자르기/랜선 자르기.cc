39c5f2a18c5bb08763cac479edc55ba408f5f011
hujeong <114281631+heehoh@users.noreply.github.com>
2024-03-30 19:43:06 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -8,46 +7,37 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int K, N;
-	cin >> K >> N;
-	vector<long long> vec;
-
-	for (int i = 0; i < K; i++)
-	{
-		long long length;
-		cin >> length;
-
-		vec.push_back(length);
-	}
-	long long min = *min_element(vec.begin(), vec.end());
-
-	long long left = 1;
-	long long right = *max_element(vec.begin(), vec.end());
-
-	long long result = 0;
-
-	while (left <= right)
-	{
-		long long mid = (left + right) / 2;
-		long long sum = 0;
-
-		for (int i = 0; i < K; i++)
-		{
-			sum += (vec[i] / mid);
-		}
-
-		if (sum >= N)
-		{
-			result = mid;
-			left = mid + 1;
- 		}
-		else
-		{
-			right = mid - 1;
-		}
-	}
-
-	cout << result;
-
-	return 0;
+    int k, n; cin >> k >> n;
+    vector<int> arr;
+    while (k-- > 0) {
+        int length; cin >> length;
+        arr.push_back(length);
+    }
+ 
+    // min max 구하기
+    int max = 0;
+    for (int i = 0; i < arr.size(); ++i) {
+        if (max < arr[i])
+            max = arr[i];
+    }
+    
+    // 이분 탐색
+    long long left = 1;
+    long long right = max;
+    long long result = 0;
+    while (left <= right) {
+        long long mid = (left + right) / 2;
+        long long sum = 0;
+        for (int i = 0; i < arr.size(); ++i) {
+            sum += (arr[i] / mid);
+        }
+        if (sum >= n) {
+            result = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    cout << result;
+    return 0;
 }
