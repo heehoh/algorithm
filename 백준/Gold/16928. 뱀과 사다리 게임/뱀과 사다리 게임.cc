fd5d5ce42c2c4fd8c0a95418d1f916314259bc31
hujeong <114281631+heehoh@users.noreply.github.com>
2024-07-20 21:11:03 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -1,49 +1,57 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
-int map[102] = {0};
-bool visit[102] = {0};
- 
-void game(int x, int c){
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, c));
-    while(!q.empty()){
-        int loc = q.front().first; // 현재 좌표
-        int cnt = q.front().second; // 카운트
-        q.pop();
- 
+
+struct Point {
+    int pos;
+    int num;
+};
+
+int map[100];
+bool visited[100];
+
+void BFS() {
+    int n = 0;
+    queue<Point> q;
+    q.push({1, 0});
+    visited[0] = true;
+    
+    while (1) {
+        Point p = q.front(); q.pop();
+
         for (int i = 1; i <= 6;i++){
-            int nx = loc + i; // 다음 좌표
+            int nx = p.pos + i; // 다음 좌표
             if(nx == 100){
-                cout << cnt+1; // 도착했으면 출력
+                cout << p.num +1; // 도착했으면 출력
                 return;
             }
             else if(nx < 100){ // 100보다 작은 좌표라면 
                 while(map[nx] != 0){ // 사다리 혹은 뱀인지 확인
                     nx = map[nx]; // 점프한 자리로 이동
                 }
-                if(!visit[nx]){ // 처음 방문한 좌표일때
-                    q.push(make_pair(nx, cnt + 1)); // 큐에 넣어줌
-                    visit[nx] = true; // 방문처리
+                if(!visited[nx]){ // 처음 방문한 좌표일때
+                    q.push({nx, p.num + 1}); // 큐에 넣어줌
+                    visited[nx] = true; // 방문처리
                 }
                 
             }
         }
     }
 }
- 
-int main(){
-    int n, m, t1, t2;
+
+int main() {
+    int N, M; cin >> N >> M;
     
-    cin >> n >> m;
-    for (int i = 0; i < n;i++){
-        cin >> t1 >> t2; 
-        map[t1] = t2; // 사다리 저장
+    while (N--) {
+        int x, y; cin >> x >> y;
+        map[x] = y;
     }
-    for (int i = 0; i < m;i++){
-        cin >> t1 >> t2;
-        map[t1] = t2; // 뱀 저장
+    
+    while (M--) {
+        int x, y; cin >> x >> y;
+        map[x] = y;
     }
-    game(1, 0);
-    return 0;
+    
+    BFS();
 }
