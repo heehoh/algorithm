7b3600ec8b65247bd9ca76022856a9997991d58f
hujeong <114281631+heehoh@users.noreply.github.com>
2024-07-29 01:45:48 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 48 ms, Memory: 8156 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -25,6 +25,8 @@ void BFS() {
     }
 }
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     cin >> N;
     for (int i = 1; i <= N - 1; i++) {
         int x, y; cin >> x >> y;
