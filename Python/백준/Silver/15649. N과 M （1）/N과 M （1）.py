89c8eee6797e530a9fea40e5d67ecab1d1d785fd
heeho <114281631+heehoh@users.noreply.github.com>
2024-09-30 14:32:21 +0900

    [Silver III] Title: N과 M (1), Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ Python/백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,17 @@
 n, m = map(int, input().split())
 visited = [False] * n
 ans = []
-def dfs(i):
-    if m == i:
+
+def dfs(dep):
+    if dep == m:
         print(' '.join(ans))
         return
-    for j in range(n):
-        if visited[j] == False:
-            ans.append(str(j+1))
-            visited[j] = True
-            dfs(i+1)
+    for num in range(n):
+        if not visited[num]:
+            ans.append(str(num + 1))
+            visited[num] = True
+            dfs(dep + 1)
             ans.pop()
-            visited[j] = False
+            visited[num] = False
 
 dfs(0)
